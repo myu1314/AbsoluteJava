fd24a38444b93247e902915287534959e8a4210c
myu1314 <142926759+myu1314@users.noreply.github.com>
2023-09-06 18:26:30 -0700

    Fixed Names

--- Ch01/ProgrammingProject02.java
+++ Ch01/ProgrammingProject02.java
@@ -5,7 +5,7 @@
   
   
 */
-public class Ch01ProgrammingProject02 {
+public class ProgrammingProject02 {
     public static void main(String[] args) {
         final double COUPONS_PER_CANDY = 10;
         final double COUPUNS_PER_GUM = 3;
