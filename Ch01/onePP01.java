a8e7850680d68ffb736ae918012fe283d08947d6
myu1314 <142926759+myu1314@users.noreply.github.com>
2023-09-19 10:19:03 -0700

    fixing names

--- Ch01/onePP01.java
+++ Ch01/onePP01.java
@@ -5,7 +5,7 @@
   
   
 */
-public class PP01 {
+public class onePP01 {
     public static void main(String[] args) {
         final double WEIGHT_KG_TO_LBS = 2.2;
         final double timeMinToHour = 60;
--- Ch02/twoPP01.java
+++ Ch02/twoPP01.java
@@ -8,7 +8,7 @@
 
 import java.util.Scanner;
 
-public class PP01 {
+public class twoPP01 {
     public static void main(String[] args) throws Exception {
         System.out.println("What is the number that you want to be square rooted?");
         Scanner keyboard = new Scanner(System.in);
