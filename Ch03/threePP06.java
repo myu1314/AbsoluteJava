c83a39f5b23f60e0b69b738ee3d7e215e121ac55
myu1314 <142926759+myu1314@users.noreply.github.com>
2023-10-16 21:58:55 -0700

    Added Unlimited

--- Ch03/threePP06.java
+++ Ch03/threePP06.java
@@ -11,47 +11,58 @@ import java.util.Scanner;
 public class threePP06 {
     public static void main(String[] args) throws Exception {
         Scanner keyboard = new Scanner(System.in);
-        final double STARTING_CRUD  = 10;
+        final double STARTING_CRUD = 10;
         final double CRUD_GROWING_DAYS = 5;
-
-        
-        double currentCrud = STARTING_CRUD;
-        double previousCrud = 0;
+        double currentCrud;
+        double previousCrud;
         double daysOfSimulation;
         double holder;
-        double totalAmount = currentCrud;
-        double daysThatHavePassed = 0;
-        System.out.println("Days that simulation run: ");
-        daysOfSimulation = keyboard.nextDouble();
-        System.out.printf("Day %2.0f: %2.0f \n",daysThatHavePassed, totalAmount);
-        while ( daysOfSimulation/CRUD_GROWING_DAYS >= 0){
-            holder = currentCrud;
-            totalAmount = currentCrud + previousCrud;
-            previousCrud = holder;
-            currentCrud = totalAmount;  
-            daysOfSimulation -= 5;
-            daysThatHavePassed += 5;
-            System.out.printf("Day %2.0f: %2.0f \n",daysThatHavePassed, totalAmount);
+        double totalAmount;
+        double daysThatHavePassed;
+        String result = "y";
+        while (result.equals("y")) {
+            currentCrud = STARTING_CRUD;
+            previousCrud = 0;
+            holder = 0;
+            totalAmount = currentCrud;
+            daysThatHavePassed = 0;
+            System.out.println("Days that simulation run: ");
+            daysOfSimulation = keyboard.nextDouble();
+            System.out.printf("Day %2.0f: %2.0f \n", daysThatHavePassed, totalAmount);
+            while (daysOfSimulation / CRUD_GROWING_DAYS >= 0) {
+                holder = currentCrud;
+                totalAmount = currentCrud + previousCrud;
+                previousCrud = holder;
+                currentCrud = totalAmount;
+                daysOfSimulation -= 5;
+                daysThatHavePassed += 5;
+                System.out.printf("Day %2.0f: %2.0f \n", daysThatHavePassed, totalAmount);
+                
+            }
+            System.out.println("Would you like to repeat the program?(y/n)");
+            keyboard.nextLine();
+            result = keyboard.nextLine();
         }
-        System.out.printf("On the final day there are %2.0f green cruds. \n", totalAmount);
         keyboard.close();
+        
     }
-   
 
 }
 /*
  * ****************************OUTPUT**************************************
- * Days that simulation run: 
+ * Days that simulation run:
  * 50
- * Day  0: 10 
- * Day  5: 10 
- * Day 10: 20 
- * Day 15: 30 
+ * Day 0: 10
+ * Day 5: 10
+ * Day 10: 20
+ * Day 15: 30
  * Day 20: 50
  * Day 25: 80
  * Day 30: 130
  * Day 35: 210
-Day 40: 340
-Day 45: 550
-Day 50: 890
+ * Day 40: 340
+ * Day 45: 550
+ * Day 50: 890
+ * Day 55: 1440
+ * On the final day there are 1440 green cruds.
  */
