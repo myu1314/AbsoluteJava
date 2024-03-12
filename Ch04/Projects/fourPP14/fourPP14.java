92ff52abe3e0d3ae74a718e3d87d1bf89fe37f65
myu1314 <142926759+myu1314@users.noreply.github.com>
2024-03-12 10:19:30 -0700

    Finish Project 14

--- Ch04/Projects/fourPP14/fourPP14.java
+++ Ch04/Projects/fourPP14/fourPP14.java
@@ -1,6 +1,5 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.util.Random;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
@@ -12,28 +11,32 @@ public class fourPP14 {
         String product3Name;
         String product4Name;
         String product5Name;
-        int product1Average = 0;
-        int product2Average = 0;
-        int product3Average = 0;
-        int product4Average = 0;
-        int product5Average = 0;
-        int product1NOR = 0;
-        int product2NOR = 0;
-        int product3NOR = 0;
-        int product4NOR = 0;
-        int product5NOR = 0;
-        int numberOfReviews = 0;
-        int numberToCheck;
+        double product1Average = 0;
+        double product2Average = 0;
+        double product3Average = 0;
+        double product4Average = 0;
+        double product5Average = 0;
+        double product1NOR = 0;
+        double product2NOR = 0;
+        double product3NOR = 0;
+        double product4NOR = 0;
+        double product5NOR = 0;
+        double numberOfReviews = 0;
+        double numberToCheck;
         String line;
         String deliminters = ",";
+
         try {
-            fileIn = new Scanner(new FileInputStream("Ch04\\rsc\\ch4_PP14.txt"));
-            line = fileIn.nextLine();
+            fileIn = new Scanner(new FileInputStream("Ch04\\rsc\\ch4_PP14.csv"));
+            fileIn.nextLine();
             while (fileIn.hasNextLine()) {
                 numberOfReviews += 1;
+                fileIn.nextLine();
+
             }
-            System.out.println(numberOfReviews);
-            System.out.println("3");
+
+            fileIn = new Scanner(new FileInputStream("Ch04\\rsc\\ch4_PP14.csv"));
+            line = fileIn.nextLine();
             StringTokenizer wordFactory = new StringTokenizer(line, deliminters);
             product1Name = wordFactory.nextToken();
             product2Name = wordFactory.nextToken();
@@ -42,46 +45,50 @@ public class fourPP14 {
             product5Name = wordFactory.nextToken();
             for (int i = 0; i < numberOfReviews; i++) {
                 line = fileIn.nextLine();
-                wordFactory  = new StringTokenizer(line, deliminters);
-                product1Average += convertNumber(wordFactory.nextToken());
+                wordFactory = new StringTokenizer(line, deliminters);
                 numberToCheck = convertNumber(wordFactory.nextToken());
-                if (numberToCheck != 0){
+                if (numberToCheck != 0) {
                     product1NOR += 1;
                 }
-                
-                product2Average += convertNumber(wordFactory.nextToken());
+                product1Average += numberToCheck;
+
                 numberToCheck = convertNumber(wordFactory.nextToken());
-                if (numberToCheck != 0){
+                if (numberToCheck != 0) {
                     product2NOR += 1;
                 }
+                product2Average += numberToCheck;
 
-                product3Average += convertNumber(wordFactory.nextToken());
                 numberToCheck = convertNumber(wordFactory.nextToken());
-                if (numberToCheck != 0){
+                if (numberToCheck != 0) {
+                    product3NOR += 1;
+                }
+                product3Average += numberToCheck;
+
+                numberToCheck = convertNumber(wordFactory.nextToken());
+                if (numberToCheck != 0) {
                     product4NOR += 1;
                 }
-               
-                product4Average += convertNumber(wordFactory.nextToken());
+                product4Average += numberToCheck;
+
                 numberToCheck = convertNumber(wordFactory.nextToken());
-                if (numberToCheck != 0){
-                    product4NOR += 1;
-                }
-             
-                product5Average += convertNumber(wordFactory.nextToken());
-                numberToCheck = convertNumber(wordFactory.nextToken());
-                if (numberToCheck != 0){
+                if (numberToCheck != 0) {
                     product5NOR += 1;
                 }
-                product1Average = product1Average/product1NOR;
-                product2Average = product2Average/product2NOR;
-                product3Average = product3Average/product3NOR;
-                product4Average = product4Average/product4NOR;
-                product5Average = product5Average/product5NOR;
-                System.out.println("The average for " + product1Name + " is " + product1Average);
-                System.out.println("The average for " + product2Name + " is " + product2Average);
-                System.out.println("The average for " + product3Name + " is " + product3Average);
+                product5Average += numberToCheck;
+
                 
+
             }
+            product1Average = product1Average / product1NOR;
+            product2Average = product2Average / product2NOR;
+            product3Average = product3Average / product3NOR;
+            product4Average = product4Average / product4NOR;
+            product5Average = product5Average / product5NOR;
+            System.out.println("The average for " + product1Name + " is " + product1Average);
+            System.out.println("The average for " + product2Name + " is " + product2Average);
+            System.out.println("The average for " + product3Name + " is " + product3Average);
+            System.out.println("The average for " + product4Name + " is " + product4Average);
+            System.out.println("The average for " + product5Name + " is " + product5Average);
 
         } catch (FileNotFoundException e) {
             System.out.println("File Not Found");
@@ -89,27 +96,24 @@ public class fourPP14 {
 
     }
 
-    static int convertNumber(String number) {
-        if (number == "1") {
-            return 1;
+    static double convertNumber(String number) {
+        System.out.println(number);
+        double numToReturn = 0;
+        if (number.equals("1")) {
+            numToReturn = 1;
+        } else if (number.equals("2")) {
+            numToReturn = 2;
+        } else if (number.equals("3")) {
+            numToReturn = 3;
+        } else if (number.equals("4")) {
+            numToReturn = 4;
+        } else if (number.equals("5")) {
+            numToReturn = 5;
+        } else if (number.equals("0")) {
+            numToReturn = 0;
+        } else {
+            numToReturn = -1;
         }
-        if (number == "2") {
-            return 2;
-        }
-        if (number == "3") {
-            return 3;
-        }
-        if (number == "4") {
-            return 4;
-        }
-        if (number == "5") {
-            return 5;
-        }
-        if (number == "0") {
-            return 0;
-        }
-        else{
-            return -1;
-        }
+        return numToReturn;
     }
 }
