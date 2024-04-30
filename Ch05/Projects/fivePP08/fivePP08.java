8db75bada5045fbc0c87789c4bb80695ef82cd82
myu1314 <142926759+myu1314@users.noreply.github.com>
2024-04-30 08:54:07 -0700

    Tested Input; Fixed Items

--- Ch05/Projects/fivePP08/fivePP08.java
+++ Ch05/Projects/fivePP08/fivePP08.java
@@ -2,31 +2,21 @@ package Ch05.Projects.fivePP08;
 import java.util.Scanner;
 public class fivePP08 {
     public static void main(String[] args) {
-        Pizza pizza1 = new Pizza("large", 2, 1, 3);
-        Pizza pizza2 = new Pizza("medium", 1, 2, 1);
-        Pizza pizza3 = new Pizza("small", 2, 3, 1);
-
-        PizzaOrder order1 = new PizzaOrder(3, pizza1, pizza2, pizza3);
-
-        PizzaOrder order2 = new PizzaOrder(order1);
-
-        order2.getPizza1().setNOCT(3); 
-
-        System.out.println("Order 1:");
-        System.out.println("Number of Pizzas: " + order1.getNumPizzas());
-        System.out.println("Pizza 1: " + order1.getPizza1().getDetails());
-        System.out.println("Pizza 2: " + order1.getPizza2().getDetails());
-        System.out.println("Pizza 3: " + order1.getPizza3().getDetails());
-        System.out.println("Total Cost: $" + order1.calcTotal());
-
-        System.out.println();
-
-        System.out.println("Order 2:");
-        System.out.println("Number of Pizzas: " + order2.getNumPizzas());
-        System.out.println("Pizza 1: " + order2.getPizza1().getDetails());
-        System.out.println("Pizza 2: " + order2.getPizza2().getDetails());
-        System.out.println("Pizza 3: " + order2.getPizza3().getDetails());
-        System.out.println("Total Cost: $" + order2.calcTotal());
-
+        Pizza pizza1 = new Pizza("large",1,0,1);// Code to create a large pizza, 1 cheese, 1 ham
+        Pizza pizza2 = new Pizza("large",2,2,0);// Code to create a medium pizza, 2 cheese,
+        // 2 pepperoni
+        PizzaOrder order1 = new PizzaOrder();// Code to create an order
+        order1.setNumPizzas(2); // 2 pizzas in the order
+        order1.setPizza1(pizza1); // Set first pizza
+        order1.setPizza2(pizza2); // Set second pizza
+        double total = order1.calcTotal(); // Should be 18+22 = 40
+        System.out.println(total); 
+        PizzaOrder order2 = new PizzaOrder(order1); // Use copy
+        // constructor
+        order2.getPizza1().setNOCT(3); // Change toppings
+        total = order2.calcTotal(); // Should be 22 + 22 = 44
+        double origTotal = order1.calcTotal(); // Should still be 40
+        System.out.println(total);
+        System.out.println(origTotal);
 }
 }
