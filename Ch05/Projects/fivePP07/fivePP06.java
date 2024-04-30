bf5d9491ded3935b53fc25b8010adc0bd2a15736
myu1314 <142926759+myu1314@users.noreply.github.com>
2024-04-30 09:43:18 -0700

    Update fivePP06.java

--- Ch05/Projects/fivePP07/fivePP06.java
+++ Ch05/Projects/fivePP07/fivePP06.java
@@ -4,7 +4,7 @@ public class fivePP06 {
     public static void main(String[] args) {
         NationalCall ncall1 = new NationalCall("6509309511", "4159129512", 10);
         NationalCall ncall2 = new NationalCall("6509309511", "4159129512", 50);
-        internationalCall icall = new internationalCall("6500123946", "4201905912", 50);
+        internationalCall icall = new internationalCall("+(1)16500123946", "+(1)4201905912", 50);
         ncall1.displayDetails();
         ncall2.displayDetails();
         icall.displayDetails();
